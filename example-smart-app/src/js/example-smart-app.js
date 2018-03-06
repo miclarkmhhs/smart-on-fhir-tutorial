90ad6daf07153de4180ef0046d2c6251c038c9d7
Mike Clark <36721012+miclarkmhhs@users.noreply.github.com>
2018-03-06 10:48:00 -0600

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -1,19 +1,25 @@
-    var loc = '';
-    var enc_text = '';
-    var usr_name = '';
+var loc = '';
+var enc_text = '';
+var usr_name = '';
 
-(function(window){
-  window.extractData = function() {
+(function(window)
+{
+  window.extractData = function() 
+  {
     var ret = $.Deferred();
-	  
-    function onError() {
+
+    function onError() 
+    {
       console.log('Loading error', arguments);
       ret.reject();
     }
 
-    function onReady(smart)  {
-      if (smart.hasOwnProperty('patient')) {                                
-        $('#resp').html(JSON.stringify(smart));
+    function onReady(smart)  
+    {
+      if (smart.hasOwnProperty('patient')) 
+      {
+        $('#resp').append(JSON.stringify(smart));
+        $('#resp').append('<BR><BR>');
         usr_name = smart.tokenResponse.username;        
         var patient = smart.patient;
         var enc_json = {'type':'Encounter', 'id': ''};
@@ -22,13 +28,16 @@
         var pt = patient.read();
         
         $.when(pt,enc).fail(onError);        
-        $.when(pt,enc).done(function(patient,encounter){ 
-        loc = encounter.data.location["0"].location.display;
-        enc_text = encounter.data.identifier["0"].value;
-        var mrn_text = patient.identifier["0"].value;
-        ret.resolve();
+        $.when(pt,enc).done(function(patient,encounter)
+	{ 
+          loc = encounter.data.location["0"].location.display;
+          enc_text = encounter.data.identifier["0"].value;
+          var mrn_text = patient.identifier["0"].value;
+          ret.resolve();
         });                                      
-      } else {
+      }
+       else 
+      {
         onError();
       }
     }
@@ -36,17 +45,21 @@
     return ret.promise();
   };
 
-  window.loadDestinationApp = function() {
+  window.loadDestinationApp = function() 
+  {
     $('#resp').show();
     $('#loading').hide();
     sendData(enc_text,loc,usr_name);
   };
 
-            function postAjax(url,data,success) {
+            function postAjax(url,data,success) 
+	    {
                 var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
                 xhr.open('POST', url,true);
-                xhr.onreadystatechange = function () {
-                    if (xhr.readyState > 3 && xhr.status == 200) {
+                xhr.onreadystatechange = function () 
+		{
+                    if (xhr.readyState > 3 && xhr.status == 200) 
+		    {
                         success(this.responseText);
                     }
                 };
@@ -59,11 +72,9 @@
                 return xhr;
             };
 
-            function createJson(enctr,loc_fac,user) {
-                // Create JSON to Post
+            function createJson(enctr,loc_fac,user) 
+	    {
                 var jsonPost = {};
-
-                // Setup API Tracking
                 jsonPost.apiTrack = {};
                 jsonPost.apiTrack.sequence = 1;
                 jsonPost.apiTrack.destination = {};
@@ -71,26 +82,21 @@
                 jsonPost.patient = {};
                 jsonPost.provider = {};
 
-                // Create Destination
                 jsonPost.apiTrack.destination.id = "perfectServe";
                 jsonPost.apiTrack.destination.name = "PerfectServe";
                 
-                // Create Source
                 jsonPost.apiTrack.source.id = "memorialHerman";
                 jsonPost.apiTrack.source.name = "Memorial Herman";
                 
-                // Setup Patient
                 jsonPost.patient.mrns = [];
                 jsonPost.patient.encounters = []; 
                
-                // Create and push patient MRN
                 var mrn = {};
                 mrn.client = "memorialHerman";
 		mrn.system = "cerner";
                 mrn.number = "";
                 jsonPost.patient.mrns.push(mrn);
 
-                // Create and push locations
 		locations = []
                 var location = {};
                 location.facility = loc_fac;
@@ -100,17 +106,13 @@
                 location.nurseOrAmbulatoryUnit = "";
 		locations.push(location);
 
-                // Create and push encounters
                 var encounter = {};
                 encounter.client = "memorialHerman";
                 encounter.fin = enctr;
 		encounter.locations = locations;
                 jsonPost.patient.encounters.push(encounter);
 
-                // Setup Provider
                 jsonPost.provider.userIds = [];
-
-		            // Create and push provider ids
                 var userId = {};
                 userId.client = "memorialHerman";
 		userId.system = "cerner";
@@ -120,12 +122,13 @@
                 return jsonPost;
             };
             
-            function sendData(enctr,loc_fac,user) {
+            function sendData(enctr,loc_fac,user) 
+	    {
                 var jsonPost = createJson(enctr, loc_fac,user);
-                postAjax('https://emr.qa-ps.com/ws/api/v1/emrlink/',jsonPost,function (data) {
-                       $('#resp').append(data);
-                       //window.location.href = data;
+                postAjax('https://emr.qa-ps.com/ws/api/v1/emrlink/',jsonPost,function (data) 
+	        {
+                   $('#resp').append(data);
+                   //window.location.href = data;
                 });
             }
-
 })(window);
